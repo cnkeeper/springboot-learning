fa663862501b444e6816e2e9eb1eeb8a3344d94e
zll <zll924924>
2018-05-16 19:03:18 +0800

    支持spring-boot-actuator健康检查，可以动态控制日志，查看服务运行状态

--- src/main/java/com/cnkeep/springboot/chapter1/section1/controller/HelloWorldController.java
+++ src/main/java/com/cnkeep/springboot/chapter1/section1/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.cnkeep.springboot.chapter1.section1.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 测试
+ *
  * @author <a href="1348555156@qq.com">LeiLi.Zhang</a>
  * @version 0.0.0
  * @date 2018/5/15
@@ -15,9 +18,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(path = "hello")
 public class HelloWorldController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldController.class);
+
     @RequestMapping(path = "/test", method = RequestMethod.GET)
     @ResponseBody
     public String hello() {
+        LOGGER.info("info.....");
+        LOGGER.warn("warnning....");
+        LOGGER.error("error......");
         return "hello";
     }
 }
