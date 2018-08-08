4b8761237f047361b80bd95659d05f1fbe3e26b4
zll <zll924924>
2018-08-08 14:36:45 +0800

    [feature](location): druid数据库连接池监控页面配置
    Desc:
    配置durid数据库连接池监控页面配置

---
+++ src/main/java/com/cnkeep/springboot/chapter1/section1/config/DruidMonitorConfiguration.java
@@ -0,0 +1,61 @@
+package com.cnkeep.springboot.chapter1.section1.config;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * 描述: Druid 数据库连接池监控页面配置
+ *
+ * @author <a href="1348555156@qq.com">LeiLi.Zhang</a>
+ * @version 0.0.0
+ * @date 2018/8/8
+ */
+@Configuration
+public class DruidMonitorConfiguration {
+
+    @NotNull
+    @NotEmpty
+    @Value("${druid.monitor.servlet-path:/druid-ui/*}")
+    private String servletPath;
+
+    @NotNull
+    @NotEmpty
+    @Value("${druid.monitor.username}")
+    private String userName;
+
+    @NotNull
+    @NotEmpty
+    @Value("${druid.monitor.password}")
+    private String password;
+
+
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean reg = new ServletRegistrationBean();
+        reg.setServlet(new StatViewServlet());
+        reg.addUrlMappings(servletPath);
+        reg.addInitParameter("loginUsername", userName);
+        reg.addInitParameter("loginPassword", userName);
+        return reg;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,"+servletPath);
+        filterRegistrationBean.addInitParameter("profileEnable", "true");
+        filterRegistrationBean.addInitParameter("principalCookieName", "USER_COOKIE");
+        filterRegistrationBean.addInitParameter("principalSessionName", "USER_SESSION");
+        return filterRegistrationBean;
+    }
+}
--- src/main/java/com/cnkeep/springboot/chapter1/section1/db/config/MasterDataSourceConfig.java
+++ src/main/java/com/cnkeep/springboot/chapter1/section1/db/config/MasterDataSourceConfig.java
@@ -83,26 +83,4 @@ public class MasterDataSourceConfig {
         factory.setMapperLocations(resolver.getResources(mapperLocations));
         return factory.getObject();
     }
-
-    @Bean
-    public ServletRegistrationBean druidServlet() {
-        ServletRegistrationBean reg = new ServletRegistrationBean();
-        reg.setServlet(new StatViewServlet());
-        reg.addUrlMappings("/druid/*");
-        reg.addInitParameter("loginUsername", "druid");
-        reg.addInitParameter("loginPassword", "druid");
-        return reg;
-    }
-
-    @Bean
-    public FilterRegistrationBean filterRegistrationBean() {
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-        filterRegistrationBean.setFilter(new WebStatFilter());
-        filterRegistrationBean.addUrlPatterns("/*");
-        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
-        filterRegistrationBean.addInitParameter("profileEnable", "true");
-        filterRegistrationBean.addInitParameter("principalCookieName", "USER_COOKIE");
-        filterRegistrationBean.addInitParameter("principalSessionName", "USER_SESSION");
-        return filterRegistrationBean;
-    }
 }
