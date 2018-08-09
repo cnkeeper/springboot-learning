56a4423aea0da1fd21a7568293d88b08e32270df
zll <zll924924>
2018-08-09 09:29:20 +0800

    [feature](location): 关闭同源策略Filter
    Desc:
    关闭同源策略Filter

--- src/main/java/com/cnkeep/springboot/chapter1/section1/security/filter/CORSFilter.java
+++ src/main/java/com/cnkeep/springboot/chapter1/section1/security/filter/CORSFilter.java
@@ -1,13 +1,18 @@
 package com.cnkeep.springboot.chapter1.section1.security.filter;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-
+/**
+ * 同源策略过滤器
+ */
 @Component
 public class CORSFilter implements Filter {
 
@@ -20,11 +25,33 @@ public class CORSFilter implements Filter {
 			throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Headers", "*");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Methods", "*");
+		response.setHeader("Access-Control-Allow-Headers", "*");
 		chain.doFilter(req, res);
 	}
 
 	@Override
 	public void destroy() {
+	}
+
+	/**
+	 * 使用spring提供的CorsFilter bean完成同源策略的问题解决
+	 * @return
+	 */
+	//@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
 	}
 
 }
